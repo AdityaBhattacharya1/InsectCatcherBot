00df831e07e35d3aeb168e3b5d4f39f2e176fcf4
Aditya Bhattacharya <69953590+AdityaBhattacharya1@users.noreply.github.com>
2021-10-12 15:07:53 +0530

    asdf

--- bot.js
+++ bot.js
@@ -13,8 +13,8 @@ function getRandomNumberBetween(min, max) {
     await page.click(
         `.insects-list > li:nth-child(${getRandomNumberBetween(1, 4)})`
     )
-    // CAUTION: uncommenting the below code will start an infinite loop and if left running for long periods of time, can cause overheating in your system.
-    // Use this at your own responsibility. The creator of the bot will not be responsible for any physical damage that may be incurred to your system while using this bot.
+    // CAUTION: uncommenting the below code will start an infinite loop and if left running for long periods of time, can cause the system to overheat.
+    // The creator of the bot will not be responsible for any damage that may be incurred to the system while using this bot.
     // while (true) {
     //     await (await page.waitForSelector('.insect')).evaluate((e) => e.click())
     // }
